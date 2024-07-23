5cb3fc2e79aef81147131d773dfc665dc5c403ae
Swan Sarli <swan.sarli@epitech.eu>
2024-07-24 00:09:19 +0200

    [ADD] Permissions + Embed

--- bot.py
+++ bot.py
@@ -17,10 +17,32 @@ async def on_ready():
     print("Bot is ready.")
     await tree.sync()
 
-@tree.command(name="test")
-async def test(interaction: discord.Interaction, message: str):
-    await client.get_channel(1263276734007087127).send(message)
-    await interaction.response.send_message("Your message has been sent! :)")
+
+
+@tree.command(name="announce", description="Announce a message to the announcements channel.")
+@app_commands.describe(
+    message = "The message you want to announce.",
+    category = "The category of the announcement."
+)
+
+async def announce(
+        interaction: discord.Interaction,
+        message: str,
+        category: str,
+        ping: bool = False
+    ):
+    if interaction.user.guild_permissions.send_messages == False:
+        await interaction.response.send_message("You do not have permission to send messages in the <#1263276734007087127> channel.", ephemeral=True)
+        return
+    if ping:
+        await client.get_channel(1263276734007087127).send("@everyone")
+    await client.get_channel(1263276734007087127).send(
+    embed=discord.Embed(
+        title = category,
+        description = message,
+        color = discord.Color.dark_purple()
+    ))
+    await interaction.response.send_message("Your message has been sent! :)", ephemeral=True)
 
 
 client.run(TOKEN)
